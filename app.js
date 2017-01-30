88ce0460698d39309399c4854118c113a44dcb84
Hanzhi Ding <leslietdhz2014@gmail.com>
2017-01-29 20:19:05 -0800

    upload v1

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "", image: ""},
+    {name: "", image: ""},
+    {name: "", image: ""}
+    ]
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started!");
+});
