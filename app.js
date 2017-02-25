73aa17e06eeb0ca23c33c444ae4c59148a80da88
Hanzhi Ding <leslietdhz2014@gmail.com>
2017-02-25 00:30:41 -0800

    update with v2 files

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser")
+var express = require("express"),
+app = express(),
+bodyParser = require("body-parser"),
+mongoose    = require("mongoose")
 
+mongoose.connect("mongodb://localhost/YelpCampV2");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "", image: ""},
-    {name: "", image: ""},
-    {name: "", image: ""}
-    ]
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
 
+/*######################*\
+##                      ##
+##    RESTful Routes    ##
+##                      ##
+\*######################*/
+
+// LANDING PAGE
 app.get("/", function(req, res){
     res.render("home");
 });
 
+// INDEX ROUTE
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, campgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("index",{campgrounds:campgrounds});
+       }
+    });
 });
 
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-});
-
+// NEW ROUTE
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/campgrounds", function(req, res){
+    // create the campground
+    Campground.create(req.body.campground, function(err, newCampground){
+        if (err){
+            console.log("error!");
+        } else {
+            res.redirect("/campgrounds"); //redirect to the index
+        }
+    });
+});
+
+// SHOW ROUTE
+app.get("/campgrounds/:id", function(req, res) {
+    // find the blog and display in the show.ejs
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log("error!");
+            res.redirect("/campgrounds");
+        } else {
+            res.render("show", {campground: campground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
